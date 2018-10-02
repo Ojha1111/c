b93495052e35e02fd2aa1e5ca46cf8521488baaf
Amit Kumar Ojha <32640038+Ojha1111@users.noreply.github.com>
2018-10-02 10:29:32 -0700

    Update Program to Check Odd or Even Using Conditional Operator.cpp

--- Program to Check Odd or Even Using Conditional Operator.cpp
+++ Program to Check Odd or Even Using Conditional Operator.cpp
@@ -5,8 +5,7 @@ int main()
 
     printf("Enter an integer: ");
     scanf("%d", &number);
-
     (number % 2 == 0) ? printf("%d is even.", number) : printf("%d is odd.", number);
 
-    return 0;
+    
 }
